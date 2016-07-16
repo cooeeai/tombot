73e4a5f8303e859e92b8ad82e9ab92b64b294bcf
Mark Moloney <markmo@me.com>
2016-07-16 18:33:59 +1000

    remove shutdown step

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -159,9 +159,9 @@ object Main extends App with Service {
 
   val bindingFuture = http.bindAndHandle(routes, "0.0.0.0", 9000)
 
-  println("Server online at http://localhost:8080/\nPress RETURN to stop...")
-  StdIn.readLine() // let it run until user presses return
-  bindingFuture
-    .flatMap(_.unbind()) // trigger unbinding from the port
-    .onComplete(_ => system.terminate()) // and shutdown when done
+//  println("Server online at http://localhost:8080/\nPress RETURN to stop...")
+//  StdIn.readLine() // let it run until user presses return
+//  bindingFuture
+//    .flatMap(_.unbind()) // trigger unbinding from the port
+//    .onComplete(_ => system.terminate()) // and shutdown when done
 }
