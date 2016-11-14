e0fd5bdfc3c83b8d3a364f92ce1f729b8b7a862e
Mark Moloney <markmo@me.com>
2016-11-14 11:38:39 +1100

    lookup child actor given intent in IntentActor

--- src/main/scala/conversationengine/IntentActor.scala
+++ src/main/scala/conversationengine/IntentActor.scala
@@ -10,13 +10,10 @@ import modules.akkaguice.{ActorInject, NamedActor}
 import services.{IntentService, UserService}
 import spray.json._
 
-import scala.concurrent.ExecutionContext.Implicits.global
-
 /**
   * Created by markmo on 18/09/2016.
   */
-class IntentActor @Inject()(sender: String,
-                            intentService: IntentService,
+class IntentActor @Inject()(intentService: IntentService,
                             userService: UserService,
                             val injector: Injector)
   extends Actor
@@ -28,15 +25,26 @@ class IntentActor @Inject()(sender: String,
     with FSM[State, Data] {
 
   import IntentActor._
+  import context.dispatcher
 
-  val child = injectActor[ConversationActor]("buy")
+  /**
+    * Create actors for each intent
+    *
+    * I considered lazy creation, however, since checking an actor's existence
+    * returns a Future, creating the actors up front results in simpler code.
+    */
+  override def preStart: Unit = {
+    injectActor[ConversationActor]("greet")
+    injectActor[ConversationActor]("buy")
+    injectActor[ConversationActor]("analyze")
+  }
 
   startWith(Active, Uninitialized)
 
   when(Active) {
 
-    case Event(ev: Confirm, _) =>
-      child ! ev
+    case Event(ev: Confirm, intent: Intent) =>
+      context.actorSelection(intent.intent) ! ev
       stay
 
     case Event(ev: TextLike, _) =>
@@ -46,38 +54,42 @@ class IntentActor @Inject()(sender: String,
 
       intentService.getIntent(text) map { meaning =>
         log.debug("received meaning:\n" + meaning.toJson.prettyPrint)
-        val intent = meaning.getIntent
-        log.debug("intent: " + intent.getOrElse("None"))
-
-        intent match {
-
-          case Some("buy") =>
-            log.debug("responding to [buy] intent")
-            child ! Qualify(platform, sender, meaning.getEntityValue("product_type"), text)
-
-          case Some("greet") =>
+        meaning.getIntent match {
+          case Some(intent@"greet") =>
             log.debug("responding to [greet] intent")
+            self ! ChangeIntent(intent)
             log.debug(s"looking up user with id [$sender]")
 
             userService.getUser(sender) match {
               case Some(user) =>
-                log.debug("user: " + user)
-                child ! Greet(platform, sender, user, text)
+                log.debug("found user: " + user)
+                context.actorSelection(intent) ! Greet(platform, sender, user, text)
+
               case None =>
                 log.warning("user not found")
             }
 
-          case Some("analyze") =>
+          case Some(intent@"buy") =>
+            log.debug("responding to [buy] intent")
+            self ! ChangeIntent(intent)
+            val productType = meaning.getEntityValue("product_type")
+            context.actorSelection(intent) ! Qualify(platform, sender, productType, text)
+
+          case Some(intent@"analyze") =>
             log.debug("responding to [analyze] intent")
-            child ! Analyze(platform, sender, text)
+            self ! ChangeIntent(intent)
+            context.actorSelection(intent) ! Analyze(platform, sender, text)
 
           case _ =>
             log.debug("responding to [unknown] intent")
-            child ! Respond(platform, sender, text)
-
+          // TODO
+          // specialized actor to handle unknown intents?
         }
       }
       stay
+
+    case Event(ChangeIntent(intent), _) =>
+      stay using Intent(intent)
 
     case Event(Deactivate, _) =>
       goto(Inactive)
@@ -93,8 +105,10 @@ class IntentActor @Inject()(sender: String,
 
   whenUnhandled {
 
-    case Event(Reset, _) =>
-      child ! Reset
+    case Event(Reset, intent: Intent) =>
+      IntentType.values foreach { x =>
+        context.actorSelection(x.toString) ! Reset
+      }
       goto(Active)
 
     // TODO
@@ -106,8 +120,11 @@ class IntentActor @Inject()(sender: String,
       context.parent ! ev
       stay
 
-    case Event(ev, _) =>
-      child ! ev
+    case Event(ev, intent: Intent) =>
+      // TODO
+      // should this be under Inactive?
+      // which non-TextLike events may be sent?
+      context.actorSelection(intent.intent) ! ev
       stay
 
   }
@@ -126,5 +143,8 @@ object IntentActor extends NamedActor {
 
   sealed trait Data
   case object Uninitialized extends Data
+  final case class Intent(intent: String) extends Data
+
+  final case class ChangeIntent(intent: String)
 
 }
---
+++ src/main/scala/conversationengine/IntentType.scala
@@ -0,0 +1,12 @@
+package conversationengine
+
+/**
+  * Created by markmo on 14/11/2016.
+  */
+object IntentType extends Enumeration {
+
+  type IntentType = Value
+
+  val GreetIntent, BuyIntent, AnalyzeIntent = Value
+
+}
